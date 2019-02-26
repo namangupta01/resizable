d2c93942fd7a15a9526f1d46a25645879ae2c290
Naman Gupta <01namangupta@gmail.com>
2019-02-26 15:20:46 +0530

    Minor bug removed and folder structure changed and published v1.0.1

--- js/resize.js
+++ js/resize.js
@@ -66,10 +66,12 @@ function makeResizableElement(id, options = {}) {
 
         });
         element.elementRightActive = elementRightActive;
-        oldClientLeft = element.getBoundingClientRect().left
+        oldClientLeft = element.getBoundingClientRect().left;
         element.style.left =  element.offsetLeft + "px";
         element.defaultPosition = element.style.position;
         element.defaultLeft = element.style.left;
+        element.previousWidth = element.getBoundingClientRect().width;
+        element.style.width = element.previousWidth;
         element.style.position = "absolute";
         elements.push(element);
         bindResizableEvents(element);
